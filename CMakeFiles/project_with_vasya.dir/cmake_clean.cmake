5ae5fc7e554c5c53796eb689103cbdba0f3c2149
vasnesterov <vas.nesterov63@gmail.com>
2022-11-18 16:37:50 +0300

    kek

---
+++ CMakeFiles/project_with_vasya.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project_with_vasya.dir/main.cpp.o"
+  "CMakeFiles/project_with_vasya.dir/main.cpp.o.d"
+  "project_with_vasya"
+  "project_with_vasya.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/project_with_vasya.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
